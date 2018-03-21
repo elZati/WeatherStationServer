fcb232a7d9bd417c846e215ea5b8a54b9e21ee8e
elZati <juha.virtanen@rxtx-designs.com>
2018-03-21 21:48:45 +0200

    88

--- Wserver_test5.cpp
+++ Wserver_test5.cpp
@@ -37,8 +37,6 @@ SensorPayload SensorNode2;
 bool fetchSensor(int nodeAddress);
 void printNodes(float dataToCompare);
 void retryFetchSensor(int nodeAddress, int max_attemptCount, int delayTime);
-//char* convertFloat(float number);
-void uploadData(void);
 /********** User Config *********/
 // Assign a unique identifier for this node, 0 or 1
 bool radioNumber = 0;
@@ -104,7 +102,6 @@ cout << "0" << ltm->tm_min << ":";
 	
 		retryFetchSensor(0, 5, 100);
 		retryFetchSensor(2, 5, 100);
-		uploadData();
 		sleep(2);
 		printNodes(SensorNode1.sensor1);
 
@@ -256,65 +253,4 @@ if (millis()-last_printout > NODE_PRINTOUT_DELAY && dataToCompare != dataToCompa
 	return;
 }
 
-void uploadData(void) {
-
-	
-	char test[]="www.google.fi";
-
-	CURL *curl;
-	CURLcode res;
-
-	curl_global_init(CURL_GLOBAL_DEFAULT);
-
-	curl = curl_easy_init();
-	if (curl) {
-		curl_easy_setopt(curl, CURLOPT_URL, test);
-
-#ifdef SKIP_PEER_VERIFICATION
-		/*
-		* If you want to connect to a site who isn't using a certificate that is
-		* signed by one of the certs in the CA bundle you have, you can skip the
-		* verification of the server's certificate. This makes the connection
-		* A LOT LESS SECURE.
-		*
-		* If you have a CA cert for the server stored someplace else than in the
-		* default bundle, then the CURLOPT_CAPATH option might come handy for
-		* you.
-		*/
-		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-#endif
-
-#ifdef SKIP_HOSTNAME_VERIFICATION
-		/*
-		* If the site you're connecting to uses a different host name that what
-		* they have mentioned in their server certificate's commonName (or
-		* subjectAltName) fields, libcurl will refuse to connect. You can skip
-		* this check, but this will make the connection less secure.
-		*/
-		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-#endif
-
-		/* Perform the request, res will get the return code */
-		res = curl_easy_perform(curl);
-		/* Check for errors */
-		if (res != CURLE_OK)
-			fprintf(stderr, "curl_easy_perform() failed: %s\n",
-			curl_easy_strerror(res));
-
-		/* always cleanup */
-		curl_easy_cleanup(curl);
-	}
-
-	curl_global_cleanup();
-
-	return;
-
-}
-
-/* char* convertFloat(float number){
-
-	char buffer[64];
-	snprintf(buffer, sizeof buffer, "%f", number);
-	return buffer;
-} */
 

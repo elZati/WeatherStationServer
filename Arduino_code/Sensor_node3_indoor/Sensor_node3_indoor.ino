f5523289f3357cdd89c8288b94c49af409270834
elZati <juha.virtanen@rxtx-designs.com>
2019-02-22 14:37:33 +0200

    Update Sensor_node3_indoor.ino

--- Arduino_code/Sensor_node3_indoor/Sensor_node3_indoor.ino
+++ Arduino_code/Sensor_node3_indoor/Sensor_node3_indoor.ino
@@ -52,14 +52,14 @@ void setup() {
   packet = {random(10, 20), 22.22, 33.33, 44.44};
 
   //Configure the humidity sensor
-  //myHumidity.begin();
+  myHumidity.begin();
   
-  pinMode(2, INPUT);
+  //pinMode(2, INPUT);
   radio.begin();
   printf_begin();
   // Set the PA Level low to prevent power supply related issues since this is a
  // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
-  radio.setPALevel(RF24_PA_HIGH);
+  radio.setPALevel(RF24_PA_MIN);
   //radio.setAutoAck(1);
   //radio.enableDynamicPayloads();
   radio.setDataRate(RF24_1MBPS);
@@ -76,6 +76,8 @@ void setup() {
   delay(1000);
   radio.printDetails();
   interrupts();
+  Serial.println("READY.");
+  //while(1);
 }
 
 void loop() {
@@ -143,7 +145,7 @@ void poll_sensors(){
 
   //packet.sensor2 = myHumidity.readHumidity();
   //packet.sensor1 = myHumidity.readTemperature();
-  packet = {random(10, 20), 22.22, 33.33, 44.44};
+  packet = {myHumidity.readTemperature(), myHumidity.readHumidity(), 33.33, 44.44};
   Serial.print("Temperature = ");
   Serial.println(packet.sensor1);
   Serial.print("Humidity = ");
